967348dd900fb396997635cd879b4ee40b4cc594
Pavitra Golchha <hello@pavi2410.me>
2021-11-08 12:55:20 +0530

    Install Tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,21 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="flex flex-col items-center justify-center min-h-screen py-8">
       <Head>
         <title>{"Pavitra's Metaverse"}</title>
         <meta name="description" content="Pavitra's Metaverse" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-16 text-center">
 
-        <h1 className={styles.title}>
-          Welcome to <a href="#">my Metaverse</a>
+        <h1 className="text-6xl leading-normal font-bold">
+          Welcome to <a href="#" className="text-blue-500 hover:underline">my Metaverse</a>
         </h1>
 
-        <p className={styles.description}>
+        <p className="my-16 text-2xl">
           Metaverse currently in development.
         </p>
 
