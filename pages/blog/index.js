6588b64286b06ae0675d74db215eb51d3b62263d
Pavitra Golchha <hello@pavi2410.me>
2021-11-08 17:07:08 +0530

    Dummy blog page

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,3 +1,15 @@
+const posts = [
+  {
+    title: 'GSoC 2020 MIT App Inventor Project VCE',
+    slug: 'gsoc-2020-mit-app-inventor-project-vce',
+  }
+]
+
 export default function Blog() {
-  return "hello"
+  return (
+    <div className="p-4 mx-auto antialiased">
+      <h1 className="leading-normal text-6xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-pink-400 to-blue-500">Blog</h1>
+      {posts.map((post,i) => (<a href={`/blog/${post.slug}`} className="underline text-blue-500 text-lg" key={i}>{post.title}</a>))}
+    </div>
+  )
 }
