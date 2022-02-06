b9806c02026758ea69c7ef33db2247cacc582240
Pavitra Golchha <hello@pavi2410.me>
2022-02-06 12:42:26 +0530

    Use simpleicons

--- pages/techstack.js
+++ pages/techstack.js
@@ -1,18 +1,18 @@
-import { chakra, Button, Container, Heading, HStack, VStack, Box, Grid, GridItem, Center, Spacer, Text, Flex } from "@chakra-ui/react";
-import { FaAndroid, FaCss3, FaCss3Alt, FaHtml5, FaJs, FaNodeJs, FaReact, FaTerminal } from "react-icons/fa";
-import { SiChakraui, SiKotlin, SiPrisma, SiSupabase, SiVite, SiMaterialdesign } from 'react-icons/si';
+import { chakra, Button, Container, Heading, HStack, VStack, Grid, GridItem, Center, Text } from "@chakra-ui/react";
+import { SiChakraui, SiKotlin, SiPrisma, SiSupabase, SiVite, SiMaterialdesign, SiWindows, SiWindowsterminal, SiJetbrains, SiDocker, SiVisualstudiocode, SiFirefoxbrowser, SiGitkraken, SiLinux, SiInsomnia, SiExpress, SiReacttable, SiReact, SiIcons8, SiHtml5, SiCss3, SiJavascript, SiAndroid, SiNodedotjs } from 'react-icons/si';
+import { FaTerminal } from "react-icons/fa";
 import { AiOutlineApi } from 'react-icons/ai';
 
 const techStacks = {
   Frontend: () => (
     <>
       <StackCell colSpan={3} bg='teal.300' icon={<SiChakraui />} title="Chakra UI" />
-      <StackCell colSpan={3} bg='red.300' icon={<FaReact />} title="React Query" />
-      <StackCell colSpan={3} color="white" bg='purple.400' icon={<FaReact />} title="React" />
+      <StackCell colSpan={3} bg='red.300' icon={<SiReacttable />} title="React Query" />
+      <StackCell colSpan={3} color="white" bg='purple.400' icon={<SiReact />} title="React" />
       <StackCell colSpan={3} color="white" bg='violet' icon={<SiVite />} title="Vite" />
-      <StackCell colSpan={2} color="white" bg='tomato' icon={<FaHtml5 />} title="HTML" />
-      <StackCell colSpan={2} color="white" bg='blue.400' icon={<FaCss3Alt />} title="CSS" />
-      <StackCell colSpan={2} color="black" bg='yellow.300' icon={<FaJs />} title="JS" />
+      <StackCell colSpan={2} color="white" bg='tomato' icon={<SiHtml5 />} title="HTML" />
+      <StackCell colSpan={2} color="white" bg='blue.400' icon={<SiCss3 />} title="CSS" />
+      <StackCell colSpan={2} color="black" bg='yellow.300' icon={<SiJavascript />} title="JS" />
     </>
   ),
   Backend: () => (
@@ -21,16 +21,16 @@ const techStacks = {
       <StackCell colSpan={2} color="white" bg='purple.400' icon={<SiPrisma />} title="JWT" />
       <StackCell colSpan={3} color="white" bg='slate.400' icon={<SiPrisma />} title="Prisma" />
       <StackCell colSpan={3} color="white" bg='green.400' icon={<SiSupabase />} title="Supabase" />
-      <StackCell colSpan={6} color="white" bg='blue.400' icon={<FaJs />} title="Express JS" />
-      <StackCell colSpan={6} color="black" bg='green.400' icon={<FaNodeJs />} title="Node JS" />
+      <StackCell colSpan={6} color="white" bg='blue.400' icon={<SiExpress />} title="Express JS" />
+      <StackCell colSpan={6} color="black" bg='green.400' icon={<SiNodedotjs />} title="Node JS" />
     </>
   ),
   Android: () => (
     <>
       <StackCell colSpan={2} color="white" bg='violet.500' icon={<SiMaterialdesign />} title="Material Design" />
       <StackCell colSpan={2} color="white" bg='orange.400' icon={<SiKotlin />} title="Ktor" />
-      <StackCell colSpan={6} color="white" bg='green.400' icon={<FaAndroid />} title="Jetpack Compose" />
-      <StackCell colSpan={3} color="white" bg='emerald.400' icon={<FaAndroid />} title="Android" />
+      <StackCell colSpan={6} color="white" bg='green.400' icon={<SiAndroid />} title="Jetpack Compose" />
+      <StackCell colSpan={3} color="white" bg='emerald.400' icon={<SiAndroid />} title="Android" />
       <StackCell colSpan={3} color="white" bg='purple.500' icon={<SiKotlin />} title="Kotlin" />
     </>
   ),
@@ -42,14 +42,14 @@ export default function TechStack() {
     <Container maxW="4xl" pb={16}>
       <Heading size="2xl" py="2rem">Tech Stacks</Heading>
       <HStack pb="4rem">
-        <StackButton icon={<FaReact />} color="purple" title="Frontend" />
-        <StackButton icon={<FaNodeJs />} color="green" title="Backend" />
-        <StackButton icon={<FaAndroid />} color="emerald" title="Android" />
+        <StackButton icon={<SiReact />} color="purple" title="Frontend" />
+        <StackButton icon={<SiNodedotjs />} color="green" title="Backend" />
+        <StackButton icon={<SiAndroid />} color="emerald" title="Android" />
       </HStack>
       <VStack spacing={16}>
-        <StackContainer title="Frontend" color="purple" icon={<FaReact />} />
-        <StackContainer title="Backend" color="green" icon={<FaNodeJs />} />
-        <StackContainer title="Android" color="emerald" icon={<FaAndroid />} />
+        <StackContainer title="Frontend" color="purple" icon={<SiReact />} />
+        <StackContainer title="Backend" color="green" icon={<SiNodedotjs />} />
+        <StackContainer title="Android" color="emerald" icon={<SiAndroid />} />
       </VStack>
     </Container>
   )
