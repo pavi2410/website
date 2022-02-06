b62e9792c079196be723cb960fea7096c7b6e714
Pavitra Golchha <hello@pavi2410.me>
2022-02-06 11:49:28 +0530

    Add android stack

--- pages/techstack.js
+++ pages/techstack.js
@@ -1,6 +1,6 @@
 import { chakra, Button, Container, Heading, HStack, VStack, Box, Grid, GridItem, Center, Spacer, Text, Flex } from "@chakra-ui/react";
 import { FaAndroid, FaCss3, FaCss3Alt, FaHtml5, FaJs, FaNodeJs, FaReact, FaTerminal } from "react-icons/fa";
-import { SiChakraui, SiPrisma, SiSupabase, SiVite } from 'react-icons/si';
+import { SiChakraui, SiKotlin, SiPrisma, SiSupabase, SiVite, SiMaterialdesign } from 'react-icons/si';
 import { AiOutlineApi } from 'react-icons/ai';
 
 const techStacks = {
@@ -25,7 +25,15 @@ const techStacks = {
       <StackCell colSpan={6} color="black" bg='green.400' icon={<FaNodeJs />} title="Node JS" />
     </>
   ),
-  Android: () => <div />,
+  Android: () => (
+    <>
+      <StackCell colSpan={2} color="white" bg='violet.500' icon={<SiMaterialdesign />} title="Material Design" />
+      <StackCell colSpan={2} color="white" bg='orange.400' icon={<SiKotlin />} title="Ktor" />
+      <StackCell colSpan={6} color="white" bg='green.400' icon={<FaAndroid />} title="Jetpack Compose" />
+      <StackCell colSpan={3} color="white" bg='emerald.400' icon={<FaAndroid />} title="Android" />
+      <StackCell colSpan={3} color="white" bg='purple.500' icon={<SiKotlin />} title="Kotlin" />
+    </>
+  ),
 }
 
 
@@ -36,12 +44,12 @@ export default function TechStack() {
       <HStack pb="4rem">
         <StackButton icon={<FaReact />} color="purple" title="Frontend" />
         <StackButton icon={<FaNodeJs />} color="green" title="Backend" />
-        <StackButton icon={<FaAndroid />} color="cyan" title="Android" />
+        <StackButton icon={<FaAndroid />} color="emerald" title="Android" />
       </HStack>
       <VStack spacing={16}>
         <StackContainer title="Frontend" color="purple" icon={<FaReact />} />
         <StackContainer title="Backend" color="green" icon={<FaNodeJs />} />
-        <StackContainer title="Android" color="cyan" icon={<FaAndroid />} />
+        <StackContainer title="Android" color="emerald" icon={<FaAndroid />} />
       </VStack>
     </Container>
   )
