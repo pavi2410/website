d95c4802515caf90aaa84c274a9a079f7958628e
Pavitra Golchha <hello@pavi2410.me>
2024-08-21 02:05:55 +0530

    fix: missing dep

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
+  site: 'https://pavi2410.me',
   redirects: {
     '/gsoc-2020-appinventor-project-vce': '/blog/gsoc-2020-appinventor-project-vce',
   }
